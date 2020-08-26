f61303d2fa0800067cf6715fe407301b9f13a03e
dirl-qrk <70246946+dirl-qrk@users.noreply.github.com>
2020-08-26 13:25:52 +0800

    Latihan Pratikum 01

---
+++ LatihanMahasiswa/src/LatihanPratikum01.java
@@ -0,0 +1,15 @@
+class pratikum{
+	String namaMahasiswa;
+	String asalMahasiswa;
+}
+public class LatihanPratikum01 {
+	public static void main(String[] args) {
+		pratikum biodataku = new pratikum();
+		
+		biodataku.namaMahasiswa = "Muh. Dirgafa Anugrah Rais";
+		biodataku.asalMahasiswa = "Makassar";
+		
+		System.out.println("Nama :" + biodataku.namaMahasiswa);
+		System.out.println("Asal :" + biodataku.asalMahasiswa);
+	}
+}
